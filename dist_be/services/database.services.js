9763d93f37317996eb2e1895974ba22122a4170c
lcaohoanq <hoangclw@gmail.com>
2025-02-16 09:41:24 +0000

    Deploy backend from main branch

--- dist_be/services/database.services.js
+++ dist_be/services/database.services.js
@@ -7,21 +7,13 @@ exports.DatabaseServices = void 0;
 const prisma_services_1 = __importDefault(require("./prisma.services"));
 const auth_1 = require("../utils/auth");
 const dotenv_1 = require("dotenv");
-const mongodb_1 = require("mongodb");
 (0, dotenv_1.config)({ path: __dirname + '/../../.env' });
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@luuhai-web-db.l3neh.mongodb.net/?retryWrites=true&w=majority&appName=luuhai-web-db`;
 const dbCollection = process.env.DB_COLLECTION;
 class DatabaseServices {
-    constructor() {
-        if (!process.env.MONGO_USER || !process.env.MONGO_PASSWORD || !process.env.DB_NAME) {
-            throw new Error('Missing required environment variables for database connection');
-        }
-        this.client = new mongodb_1.MongoClient(uri);
-        this.db = this.client.db(process.env.DB_NAME);
-    }
     async connect() {
         try {
-            await this.db.command({ ping: 1 });
+            await prisma_services_1.default.$connect();
         }
         catch (error) {
             console.log(error);
@@ -29,7 +21,7 @@ class DatabaseServices {
         }
     }
     // User services
-    createUser(username, password) {
+    async createUser(username, password) {
         const { hashedPassword, salt } = (0, auth_1.hashPassword)(password);
         return prisma_services_1.default.user.create({
             data: {
@@ -69,6 +61,9 @@ class DatabaseServices {
                 products: true,
             },
         });
+    }
+    async disconnect() {
+        await prisma_services_1.default.$disconnect();
     }
 }
 exports.DatabaseServices = DatabaseServices;
--- dist_be/services/prisma.services.js
+++ dist_be/services/prisma.services.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
+// Create a singleton instance
+const prisma = global.prisma || new client_1.PrismaClient();
+if (process.env.NODE_ENV !== 'production') {
+    global.prisma = prisma;
+}
 exports.default = prisma;
